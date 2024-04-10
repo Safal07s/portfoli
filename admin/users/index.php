c1980248519e6eb39317a2f8c55a2abe76a6555a
Safal07s <147456530+Safal07s@users.noreply.github.com>
2024-04-10 11:17:40 +0545

    This is first comment

---
+++ admin/users/index.php
@@ -0,0 +1,71 @@
+
+<?php require('../includes/header.php'); ?>
+
+<?php require('../includes/navbar.php'); ?>
+
+<?php require('../includes/sidebar.php'); ?>
+ 
+ 
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Manage Users</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item">Users</li>
+          <li class="breadcrumb-item active">Manage Users</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Manage Users</h5>
+
+              <!-- Table with stripped rows -->
+              <table class="table datatable">
+                <thead>
+                  <tr>
+                    <th>
+                      <b>N</b>ame
+                    </th>
+                    <th>Ext.</th>
+                    <th>City</th>
+                    <th data-type="date" data-format="YYYY/DD/MM">Start Date</th>
+                    <th>Completion</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Unity Pugh</td>
+                    <td>9958</td>
+                    <td>Curicó</td>
+                    <td>2005/02/11</td>
+                    <td>37%</td>
+                  </tr>
+                  <tr>
+                    <td>Zelenia Roman</td>
+                    <td>7516</td>
+                    <td>Redwater</td>
+                    <td>2012/03/03</td>
+                    <td>31%</td>
+                  </tr>
+                </tbody>
+              </table>
+              <!-- End Table with stripped rows -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+<?php require('../includes/footer.php'); ?>
+ 
